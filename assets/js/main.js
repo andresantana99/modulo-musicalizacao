e30b72d376ecf5d324c0c2f5e0bd9bd2be751efb
André Santana <andre.mendonca.santana@itec.ufpa.br>
2023-03-09 22:46:59 -0300

    Avanços na reprodução da tela inicial do Duolingo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,18 @@
 $(document).ready(function () {
-    console.log("teste");
-    $("#btn-botao").click(function (e) { 
-        e.preventDefault();
-        console.log(x);
-    });
-});
+  console.log("teste");
+  $("#btn-botao").click(function (e) {
+    e.preventDefault();
+    console.log(x);
+  });
 
+  var toggle = document.getElementById("toggle");
+  var exemplo = document.getElementById("exemplo");
+
+  toggle.onclick = function () {
+    if (exemplo.className == "hide") {
+      exemplo.className = "";
+    } else {
+      exemplo.className = "hide";
+    }
+  };
+});
