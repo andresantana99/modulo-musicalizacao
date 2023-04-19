6021d1a7ccb406d11b09b424fda844fa5a513c78
André Santana <andre.mendonca.santana@itec.ufpa.br>
2023-04-19 19:31:37 -0300

    Salvando avanço sobre o back da aplicação

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,51 @@
 $(document).ready(function () {
-  $("#btn-EtapaInicial").click(function (e) { 
+  //$("#conteudo").load("views/shared/mainpage.html");
+  let ambiente;
+
+  if (window.location.host == '127.0.0.1:5500' || window.location.host == 'localhost') {
+    ambiente = "dev";
+  } else if (window.location.host == 'andresantana99.github.io') {
+    ambiente = "prd";
+  }
+
+  if (window.localStorage) {
+    // Suporte ao localStorage
+    console.log(window.location.href);
+    console.log(window.location.protocol);
+    console.log(window.location.host);
+    console.log(window.location.pathname);
+  }
+
+  //let key = "item 1";
+  // localStorage.setItem(key, "value");
+
+  //let myItem = localStorage.getItem(key);
+
+  //localStorage.removeItem(key);
+  // localStorage.clear();
+
+  //console.log(myItem);
+
+  // Criar item:
+  //let myObj = { name: "Gabriel", age: 25 };
+  //localStorage.setItem(key, JSON.stringify(myObj));
+
+  // Ler item:
+  //let myItem = JSON.parse(localStorage.getItem(key));
+
+  // for (let i = 0; i < localStorage.length; i++){
+  //   let key = localStorage.key(i);
+  //   let value = localStorage.getItem(key);
+  //   console.log(key, value);
+  // }
+
+  $("#btn-EtapaInicial").click(function (e) {
     e.preventDefault();
     window.location.href = "../views/etapa1/index.html";
   });
 
-  $("#btn-SelecaoEtapa").click(function (e) { 
+  $("#btn-SelecaoEtapa").click(function (e) {
     e.preventDefault();
     window.location.href = "../views/selecaoEtapa/index.html";
   });
-
-
 });
