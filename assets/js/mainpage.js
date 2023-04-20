55c2bf4647eb8d234361e9d08b85b425c1ed1a10
Leandro Henrique <leandro.pinheiro1427@gmail.com>
2023-04-20 09:46:27 -0300

    Correção de caminho relativo

--- assets/js/mainpage.js
+++ assets/js/mainpage.js
@@ -2,12 +2,12 @@ $(document).ready(function () {
   
     $("#btn-EtapaInicial").click(function (e) {
       e.preventDefault();
-      window.location.href = "../views/etapa1/index.html";
+      window.location.href = "./views/etapa1/index.html";
     });
   
     $("#btn-SelecaoEtapa").click(function (e) {
       e.preventDefault();
-      window.location.href = "../views/selecaoEtapa/index.html";
+      window.location.href = "./views/selecaoEtapa/index.html";
     });
   });
   
