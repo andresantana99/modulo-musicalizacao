d72c4072266039b38c4d61d6523b88befc23d3f2
Leandro Henrique <leandro.pinheiro1427@gmail.com>
2023-12-11 13:40:47 -0300

    Adiciona lib sweetalert.js, ajusta tela inicial da etapa 3 e altera nomes de variáveis no protótipo de metrônomo

--- assets/js/etapa3.js
+++ assets/js/etapa3.js
@@ -1,3 +1,19 @@
 $(document).ready(function () {
-  
+  $("#home").click(function (e) {
+    e.preventDefault();
+    Swal.fire({
+      title: "Você deseja ir para a Tela Inicial?",
+      text: "O seu progresso nessa etapa será perdido!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Confirmar",
+      cancelButtonText: "Cancelar",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        window.location.href = "../../index.html";
+      }
+    });
+  });
 });
