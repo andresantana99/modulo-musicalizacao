dcf8198d9513670614dd3b80ac0d023bc78ea806
Leandro Henrique <leandro.pinheiro1427@gmail.com>
2023-12-11 22:36:16 -0300

    Implementa as 7 telas da etapa 3

--- assets/js/etapa3.js
+++ assets/js/etapa3.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
+
+  // Modal de confirmação para retorno à tela principal
   $("#home").click(function (e) {
     e.preventDefault();
     Swal.fire({
       title: "Você deseja ir para a Tela Inicial?",
-      text: "O seu progresso nessa etapa será perdido!",
+      text: "O seu progresso nessa etapa poderá ser perdido!",
       icon: "warning",
       showCancelButton: true,
       confirmButtonColor: "#3085d6",
@@ -16,4 +18,38 @@ $(document).ready(function () {
       }
     });
   });
+
+  //localStorage.setItem("pagina_etapa3", 1);
+
+  if (localStorage.getItem("pagina_etapa3") === null) {
+    localStorage.setItem("pagina_etapa3", 1);
+    $("#conteudo").load(`pagina_1_etapa3.html`);
+  } else {
+    $("#conteudo").load(`pagina_${localStorage.getItem("pagina_etapa3")}_etapa3.html`);
+  }
+
+  // #region Funcionalidade de avançar ou retornar a tela
+  $("#btnVoltar").click((e) => {
+    e.preventDefault();
+    if (parseInt(localStorage.getItem("pagina_etapa3")) <= 1) {
+      localStorage.setItem("pagina_etapa3", 1);
+      window.location.href = "../../views/selecaoEtapa/index.html";
+    } else {
+      localStorage.setItem("pagina_etapa3", parseInt(localStorage.getItem("pagina_etapa3")) - 1);
+      $("#conteudo").load(`pagina_${localStorage.getItem("pagina_etapa3")}_etapa3.html`);
+    }
+  });
+
+  $("#btnAvancar").click((e) => {
+    console.log(localStorage.getItem("pagina_etapa3"));
+    e.preventDefault();
+    if (parseInt(localStorage.getItem("pagina_etapa3")) >= 7) {
+      localStorage.setItem("pagina_etapa3", 1);
+      window.location.href = "../../views/selecaoEtapa/index.html";
+    } else {
+      localStorage.setItem("pagina_etapa3", parseInt(localStorage.getItem("pagina_etapa3")) + 1);
+      $("#conteudo").load(`pagina_${localStorage.getItem("pagina_etapa3")}_etapa3.html`);
+    }
+  });
+  // #endregion
 });
