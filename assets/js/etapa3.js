06b16164894749009c59b60c8bd3d30cf92195e8
Leandro Henrique <leandro.pinheiro1427@gmail.com>
2023-12-14 17:31:00 -0300

    Altera tema do sweetalert e organiza as pastas de arquivos estáticos do sweetalert

--- assets/js/etapa3.js
+++ assets/js/etapa3.js
@@ -42,7 +42,7 @@ $(document).ready(function () {
 
   $("#btnAvancar").click((e) => {
     e.preventDefault();
-    if (parseInt(localStorage.getItem("pagina_etapa3")) >= 8) {
+    if (parseInt(localStorage.getItem("pagina_etapa3")) >= 9) {
       localStorage.setItem("pagina_etapa3", 1);
       window.location.href = "../../views/selecaoEtapa/index.html";
     } else {
